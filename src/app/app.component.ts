38036fb4c401c3e16a2e1ce5743618d45e5e871b
PRINCE RAJ <rajprince031@gmail.com>
2025-03-07 12:43:27 +0530

    [Prince Raj] add . To display Hello from BridgeLabz using AngularJS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import {FormsModule} from '@angular/forms';
+import {CommonModule} from '@angular/common';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+
 })
 export class AppComponent {
   title = 'Hello From BridgeLabz';
   imageUrl = '../assets/BL_logo_square_png.png';
+  userName : string="";
   onClick(){
     console.log("Button clicked");
     window.open("https://www.bridgeLabz.com",'_blank');
