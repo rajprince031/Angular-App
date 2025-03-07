668741a43ba843c7475cd5fb8b70d41e106c80b3
PRINCE RAJ <rajprince031@gmail.com>
2025-03-07 12:51:58 +0530

    [Prince Raj] add .  using AngularJS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,8 +13,18 @@ export class AppComponent {
   title = 'Hello From BridgeLabz';
   imageUrl = '../assets/BL_logo_square_png.png';
   userName : string="";
+  errorMessage : string="";
   onClick(){
     console.log("Button clicked");
     window.open("https://www.bridgeLabz.com",'_blank');
+  }
+  nameValidate(){
+    if(!this.userName){
+      this.errorMessage = "Name is Required";
+    }else if(!/^[A-Z][a-zA-Z]{2,}/.test(this.userName)){
+      this.errorMessage = "Name starts with capital letter and minimum character require is 3";
+    }else{
+      this.errorMessage = "";
+    }
   }
 }
